b1f7ecb4e8dc0948f46593901f945111d3ee0874
opedrohra <pedrorabeo0202@gmail.com>
2026-06-17 09:41:40 -0300

    insercao_de_dados

--- projeto/crud_produtos.c
+++ projeto/crud_produtos.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     
@@ -9,19 +10,27 @@ int main() {
     int registros = 0; // quantidade de produtos
     
     //Base de dados
-    struct produtos {
+    struct produtos_t {
         int id;
-        char nome[50];
-        char descricao[100];
+        char nome[51];
+        char descricao[101];
         float preco;
-    } produto;
-
+    
+    };
+struct produtos_t produtos[10];
+int quantidade = 0;
+int id = 0;
+      
+    
+    
+    
+    
     // menu
 
-
+        do{
         system("clear");
         printf("\n========================================\n");
-        printf("          SISTEMA DE PRODUTOS           \n");
+        printf("      Mercado do Pedro e Gabriel         \n");
         printf("========================================\n");
         printf("1. Cadastrar Produto\n");
         printf("2. Listar Produtos\n");
@@ -30,18 +39,93 @@ int main() {
         printf("5. Sair\n");
         printf("========================================\n");
         printf("Escolha uma opcao: ");
-  
-        // Validação de entrada
-        if (scanf("%d", &menu) >5) {
-            printf("Entrada invalida! Digite um numero.\n");
-            while(getchar() != '\n'); // Limpa o buffer
-
-        }
+        scanf("%i", &menu);
+         while(getchar() != '\n');
         
-        switch (menu)
+        
+        
+            /* code */
+       
+        
+    
+            switch (menu)
         {
-        case 1: system("clear");
-        printf("Vamos cadastrar o produto.");break;
+        case 1: {system("clear");
+        
+         
+            
+            int idDigitado = 0;
+            int idDuplicado;
+   if (quantidade >= 10) {
+                printf("Erro: limite maximo de 10 produtos atingido.\n");
+                printf("Pressione ENTER para voltar ao menu.");
+                getchar();
+                break;
+            }
+                  
+  
+  
+            do{
+  
+   
+    idDuplicado=0;
+   
+            
+            printf("Vamos cadastrar o produto.\n");
+        
+        printf("Digite o ID desejado do produto que voce quer cadastrar\n");
+        
+        scanf("%i", &idDigitado);
+        
+        while(getchar() != '\n');
+
+    if (idDigitado <= 0) {
+        printf("Erro: o ID deve ser um numero inteiro positivo.\n\n");
+    } else{
+    
+            
+    for (int i = 0; i < quantidade; i++) {
+     if(produtos[i].id == idDigitado){
+            idDuplicado=1;
+     }
+      } 
+        
+       if( idDuplicado == 1){   
+      printf("Id duplicado tente novamente\n");
+       } 
+       
+
+    } }while (idDigitado <= 0 || idDuplicado == 1);
+produtos[quantidade].id = idDigitado;
+        
+        
+        printf("Digite o nome do produto da mercearia que voce quer adicionar a sacola:\n ");
+        scanf("%[^\n]s", produtos[quantidade].nome);
+        while(getchar() != '\n');
+        printf("Agora digite o preco do produto:");
+        scanf("%f", &produtos[quantidade].preco);
+         while(getchar() != '\n'); 
+         
+            if(produtos[quantidade].preco <0){
+        printf("Erro, preco nao pode ser negativo\n");
+        
+        }else  
+   
+        
+           
+        quantidade++;
+          do{
+            for (int i = 0; i<quantidade ; i++)
+            {
+            printf("Produto %i : %s - %.2f\n", i+1,
+            produtos[i].nome, produtos[i].preco);
+             }
+            }while(produtos[quantidade].preco < 0);
+    
+        //     printf("Voce chegou no limite de produtos cadastrados, pressione ENTER para voltar ao menu principal.");
+        // getchar();
+            
+        };break;
         case 2: system("clear");
         printf("Listagem de produtos."); break;
         case 3: system("clear"); 
@@ -50,16 +134,12 @@ int main() {
         printf("Remoção de produtos."); break;
         case 5: system("clear");
         printf("Obrigado por usar o programa, adeus."); break;
-
+        default:
+        printf("Opa opcao invalida tente novamente.\n");}
         
-        default: 
-        // do { 
-        printf("Opa opcao invalida tente novamente.\n");break;} 
-        //Tentei de alguma forma fazer o loop com a outra parte do codigo mas nao consegui
-    //     printf("Pressione ENTER para continuar...\n");  
-    //      getchar();
-    // }   while(menu !=0); break;
-        // }
+       } while(menu !=5);
+    
+
     
 
 
